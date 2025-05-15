ef799a5061af2a05801310b35ac3573b33c65b31
Eden Reich <eden.reich@gmail.com>
2025-05-15 12:00:09 +0000

    fix: Update version information handling in build process and improve comments
    Signed-off-by: Eden Reich <eden.reich@gmail.com>

--- config/version.go
+++ config/version.go
@@ -1,7 +1,18 @@
 package config
 
+// Version information set by semantic release
+// These variables can be overridden during build time using ldflags
+
 var (
-	Version   = "dev"
+	// Version holds the current version of n8n-cli
+	// This will be replaced during semantic release with the actual version
+	Version = "dev"
+
+	// BuildDate holds the date when the binary was built
+	// This will be replaced during build with the actual build date
 	BuildDate = "unknown"
-	Commit    = "none"
+
+	// Commit holds the git commit hash used to build the binary
+	// This will be replaced during build with the actual git commit hash
+	Commit = "none"
 )
